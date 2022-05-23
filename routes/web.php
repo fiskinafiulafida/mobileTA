bfcc4ea7b7d39745a881134b858ba70236bb786f
fiskinafiulafida <fiskinafiul@gmail.com>
2022-05-23 22:02:27 +0700

    View Login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// // Login 
+Route::get('/loginAdmin', [LoginController::class, 'index']);
