d91cc183c7df0240a7a2a4d4f4fa6d19ff4f9eaf
elizabethra16 <elizabeth160102@gmail.com>
2022-06-26 19:53:51 +0700

    pemasukan terbaru

--- app/Models/Pemasukan.php
+++ app/Models/Pemasukan.php
@@ -1,22 +1,15 @@
 <?php
+
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 class Pemasukan extends Model
 {
     use HasFactory;
     protected $table = "pemasukan";
 
     protected $fillable = [
-        'id',
-        'nama',
-        'deskripsi',
-        'gambar',
         'gambar', 'nama', 'deskripsi'
     ];
-
-    public function pemasukan()
-    {
-        return $this->hasMany(pemasukan::class);
-    }
 }
